60bc02e9787ac3f5ac95e335541522b01320c969
JitaDesWadyas <167089111+JitaDesWadyas@users.noreply.github.com>
2026-06-27 11:51:07 +0200

    Cache complete card theme assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="storia52-v4";
-const FILES=["./","index.html","style.css","update.css","app.js","ui-core.js","ui-modes.js","icon.svg","manifest.webmanifest"];
+const CACHE="storia52-v5";
+const FILES=["./","index.html","style.css","update.css","editorial-polish.css","app.js","ui-core.js","ui-modes.js","icon.svg","manifest.webmanifest"];
 
 self.addEventListener("install",event=>{
   self.skipWaiting();
