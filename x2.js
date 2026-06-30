3b3f98fcfcac4903270900b8fe5e24521aa51033
JitaDesWadyas <167089111+JitaDesWadyas@users.noreply.github.com>
2026-06-30 11:56:24 +0200

    Riduci gli strumenti contestuali

--- x2.js
+++ x2.js
@@ -1,2 +1,12 @@
 'use strict';
-window.testRules = 'Ogni giocatore riceve cinque carte fisiche.';
+(() => {
+  const game = document.querySelector('#game');
+  if (!game) return;
+  const clean = () => {
+    game.querySelector('.scene-example')?.remove();
+    game.querySelector('#helpCard')?.remove();
+    game.querySelector('#helpPair')?.remove();
+  };
+  new MutationObserver(clean).observe(game, { childList: true, subtree: true });
+  clean();
+})();
