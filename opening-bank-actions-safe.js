ade41388ac914b53e83696d3547bb43133003567
JitaDesWadyas <167089111+JitaDesWadyas@users.noreply.github.com>
2026-06-30 14:13:29 +0200

    Aggiungi azioni semplici per l’incipit

---
+++ opening-bank-actions-safe.js
@@ -0,0 +1,3 @@
+'use strict';
+window.OPENING_BANK=window.OPENING_BANK||{};
+window.OPENING_BANK.actions=['chiama una persona importante','raggiunge il luogo indicato','consegna una vecchia lettera','ripara il macchinario','riunisce due persone in conflitto','segue gli indizi scritti sul biglietto','recupera la borsa dimenticata','avvisa il gruppo del cambiamento','ascolta la registrazione fino alla fine','rinuncia al proprio posto per aiutare qualcuno','apre la cartella ricevuta','prepara il passo successivo'];
