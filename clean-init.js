19b503f3d7d58d17ed66be25868e8e23fce291a5
JitaDesWadyas <167089111+JitaDesWadyas@users.noreply.github.com>
2026-07-01 19:31:31 +0200

    Apri regole e uscita dalla testata

--- clean-init.js
+++ clean-init.js
@@ -2,16 +2,8 @@
 (() => {
   const S = window.S52;
   initTheme();
-  S.initArchive();
-  S.renderRulesPage();
-  document.querySelectorAll('.main-nav button').forEach(button => button.addEventListener('click', () => {
-    const page = button.dataset.page;
-    if (page === 'play') S.renderHome();
-    else {
-      if (page === 'rules') S.renderRulesPage();
-      openPage(page);
-    }
-  }));
+  document.querySelector('#headerRules')?.addEventListener('click', () => S.openRulesModal());
+  document.querySelector('#headerExit')?.addEventListener('click', () => S.openExitModal());
   if (!S.renderInviteFromUrl()) S.renderHome();
   if ('serviceWorker' in navigator) navigator.serviceWorker.register('sw.js').catch(() => {});
 })();
