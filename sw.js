689cbb2b81c3d2b6b84392a3a2ea6b61a589e1ee
JitaDesWadyas <167089111+JitaDesWadyas@users.noreply.github.com>
2026-07-22 10:19:08 +0200

    Update the offline shell for virtual cards v31

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const CACHE_PREFIX = 'epoi-';
-const SHELL_CACHE = `${CACHE_PREFIX}shell-v30`;
-const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-v30`;
+const SHELL_CACHE = `${CACHE_PREFIX}shell-v31`;
+const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-v31`;
 const CORE_FILES = [
   './', './index.html', './privacy.html', './copyright.html',
   './clean-app.css', './refine-flow.css', './home-product.css', './app-polish.css', './home-fixes.css',
