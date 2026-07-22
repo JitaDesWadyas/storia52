0464392c707cb575d5845772645955e6f2743c7f
JitaDesWadyas <167089111+JitaDesWadyas@users.noreply.github.com>
2026-07-22 11:32:35 +0200

    Bump PWA cache for mobile card table

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const CACHE_PREFIX = 'epoi-';
-const SHELL_CACHE = `${CACHE_PREFIX}shell-v32`;
-const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-v32`;
+const SHELL_CACHE = `${CACHE_PREFIX}shell-v33`;
+const RUNTIME_CACHE = `${CACHE_PREFIX}runtime-v33`;
 const CORE_FILES = [
   './', './index.html', './privacy.html', './copyright.html',
   './clean-app.css', './refine-flow.css', './home-product.css', './app-polish.css', './home-fixes.css',
